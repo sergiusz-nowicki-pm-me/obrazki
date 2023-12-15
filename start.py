7d49e1ebeeb7e518dbf0d3ee3c0639892bf7498b
your name <test@test.com>
2023-12-15 17:17:08 +0100

    .

--- start.py
+++ start.py
@@ -5,10 +5,17 @@
 # 3. Wczytań wybrane pliki po kolei i przetworzyć
 # 4. Zapisać pliki do podkatalogu
 
+
 import sys
 import os
 import imageio
+import PIL
 
+conversion = None
+resize = None
+rotate = None
+
+sciezkaKataloguWyjsciowego = 'tmp'
 sciezka = "."
 obslugiwaneTypyPlikow = ['.jpg', '.jpeg', '.png']
 
@@ -17,31 +24,57 @@ if len(sys.argv) > 1:
         print("Wczytuje parametr", paramNum, f'o wartości "{sys.argv[paramNum]}"')
         if sys.argv[paramNum].startswith("-p="):
             sciezka = sys.argv[paramNum][3:]
-            print(f'Podano ścieżkę do katalogu ropboczego "{sciezka}"')
+            print(f'Podano ścieżkę do katalogu roboczego "{sciezka}"')
+        elif sys.argv[paramNum].startswith("-o="):
+            sciezkaKataloguWyjsciowego = sys.argv[paramNum][3:]
+            print(f'Podano ścieżkę do katalogu wyjscia "{sciezkaKataloguWyjsciowego}"')
+        elif sys.argv[paramNum].startswith("-c="):
+            conversion = sys.argv[paramNum][3:]
+            print(f'Podano wartość konwersji')
+        elif sys.argv[paramNum].startswith("-s="):
+            tekst = sys.argv[paramNum][3:].split(":")
+            if len(tekst) != 2:
+                print("Podano bledny wymiar -s= . Nalezy podac szerokosc:wysokosc")
+                exit()
+            szerokosc,wysokosc = tekst
+            try:
+                resize = (int(szerokosc),int(wysokosc))
+            except:
+                print("Wymiary musza byc liczba calkowita")
+                exit()
+            print(f'Podano ścieżkę do katalogu wyjscia "{sciezkaKataloguWyjsciowego}"')
         else:
             print(f'Podano nieobsługiwany typ parametru "{sys.argv[paramNum]}"')
-
-zawartoscKatalogu = [wpis for wpis in os.listdir(sciezka) if os.path.isfile(os.path.join(sciezka, wpis))]
+os.chdir(sciezka)
+zawartoscKatalogu = [wpis for wpis in os.listdir(".") if os.path.isfile(wpis)]
 print("pliki w katalogu", zawartoscKatalogu)
 plikiGraficzne = [wpis for wpis in zawartoscKatalogu if wpis[wpis.rfind('.'):] in obslugiwaneTypyPlikow]
 print("pliki graficzne w katalogu", plikiGraficzne)
 
-nazwaKataloguWyjsciowego = 'tmp'
-katalogWyjsciowy = ''
-index = 0
-while True:
-    if index > 0:
-        katalogWyjsciowy = os.path.join(sciezka, nazwaKataloguWyjsciowego + str(index))
-    else:
-        katalogWyjsciowy = os.path.join(sciezka, nazwaKataloguWyjsciowego)
-    index += 1
 
-    if os.path.exists(katalogWyjsciowy):
-        continue
-    else:
-        os.mkdir(katalogWyjsciowy)
-        break
+# index = 0
+# while True:
+#     if index > 0:
+#         katalogWyjsciowy = os.path.join(sciezka, nazwaKataloguWyjsciowego + str(index))
+#     else:
+#         katalogWyjsciowy = os.path.join(sciezka, nazwaKataloguWyjsciowego)
+#     index += 1
+
+    # if os.path.exists(katalogWyjsciowy):
+    #     continue
+    # else:
+    #     os.mkdir(katalogWyjsciowy)
+    #     break
+if not os.path.exists(sciezkaKataloguWyjsciowego):
+    os.makedirs(sciezkaKataloguWyjsciowego)
 
 for imageFile in plikiGraficzne:
-    img = imageio.v3.imread(os.path.join(sciezka, imageFile))
-    imageio.v3.imwrite(os.path.join(katalogWyjsciowy, imageFile + '.png'), img)
+    img = imageio.v3.imread(imageFile)
+    image = PIL.Image.fromarray(img)
+    if resize != None:
+        image = image.resize(resize)
+    if conversion != None:
+        image = image.convert(conversion)  
+    # image.show()
+    image.save(os.path.join(sciezkaKataloguWyjsciowego, imageFile))
+   # imageio.v3.imwrite(os.path.join(katalogWyjsciowy, imageFile + '.png'), img)
